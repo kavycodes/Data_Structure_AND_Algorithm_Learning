0eef46c73c3582f4744ff5f0e067a766917ea778
Kavya Gupta <kavygupta0605@gmail.com>
2026-04-22 21:41:34 +0530

    Added Problem_02

---
+++ Basics/Function/Problem_02.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+bool isEven(int a){
+    if(a&1){
+        return 0;
+    }
+    else{
+        return 1;
+    }
+}
+
+int main() {
+    int num;
+    cin>>num;
+    cout<<"(1)Even or (0)Odd:"<<isEven(num);
+    return 0;
+}
